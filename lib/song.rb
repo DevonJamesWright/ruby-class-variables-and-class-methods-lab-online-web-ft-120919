6b772472d0c0ea49986873b826052591eff313f9
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-19 20:08:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,5 +35,16 @@ class Song
       end
       genre_hash
   end
+  def self.artist_count
+    artist_hash = {}
+      @@artists.each do |artist|
+        if artist_hash[artist]
+          artist_hash[artist] += 1 
+        else 
+          artist_hash[genre] = 1 
+        end
+      end
+      artist_hash
+  end
 end 
 
