b22cfa7cedfc39697b2885a39d9e27cab2389e81
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-19 20:04:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
   def self.genres
    @@genres.uniq
   end 
-  def self.genre_count(songs)
+  def self.genre_count(genre)
     genre_hash = {}
       @@genres.each do |genre|
         if genre_hash[genre]
